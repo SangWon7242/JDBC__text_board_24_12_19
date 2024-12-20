f1d045c294b432d17a55e77471ea769ddd474f96
SangWon7242 <gotkdqja@gmail.com>
2024-12-20 20:49:32 +0900

    JDBC SELECT 테스트

--- src/main/java/com/sbs/board/article/Article.java
+++ src/main/java/com/sbs/board/article/Article.java
@@ -1,33 +1,64 @@
 package com.sbs.board.article;
 
+import java.time.LocalDateTime;
+
 public class Article {
   private final int id;
+  private LocalDateTime regDate;
+  private LocalDateTime updateDate;
   private String subject;
   private String content;
+
   public Article(int id, String subject, String content) {
     this.id = id;
     this.subject = subject;
     this.content = content;
   }
+
+  public Article(int id, LocalDateTime regDate, LocalDateTime updateDate, String subject, String content) {
+    this.id = id;
+    this.regDate = regDate;
+    this.updateDate = updateDate;
+    this.subject = subject;
+    this.content = content;
+  }
+
   public int getId() {
     return id;
   }
+
+  public LocalDateTime getRegDate() {
+    return regDate;
+  }
+
+  public LocalDateTime getUpdateDate() {
+    return updateDate;
+  }
+
   public String getSubject() {
     return subject;
   }
   public String getContent() {
     return content;
   }
+
+  public void setUpdateDate(LocalDateTime updateDate) {
+    this.updateDate = updateDate;
+  }
+
   public void setSubject(String subject) {
     this.subject = subject;
   }
   public void setContent(String content) {
     this.content = content;
   }
+
   @Override
   public String toString() {
     return "Article{" +
         "id=" + id +
+        ", regDate='" + regDate + '\'' +
+        ", updateDate='" + updateDate + '\'' +
         ", subject='" + subject + '\'' +
         ", content='" + content + '\'' +
         '}';
---
+++ src/test/java/JDBCSelectTest.java
@@ -0,0 +1,69 @@
+import com.sbs.board.article.Article;
+
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCSelectTest {
+  // JDBC URL, 사용자명, 비밀번호를 설정합니다.
+  private static final String DB_URL = "jdbc:mysql://localhost:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeBehavior=convertToNull";
+  private static final String DB_USER = "root"; // MariaDB 사용자 이름
+  private static final String DB_PASSWORD = ""; // MariaDB 비밀번호
+
+  public static void main(String[] args) {
+    // MariaDB에 연결을 시도합니다.
+    Connection conn = null;
+    PreparedStatement pstat = null;
+    ResultSet rs = null;
+
+    List<Article> articles = new ArrayList<>();
+
+    try {
+      // JDBC 드라이버 로드
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+      System.out.println("데이터베이스 연결 성공");
+
+      String sql = "SELECT *";
+      sql += " FROM article";
+      sql += " ORDER BY id DESC;";
+
+      pstat = conn.prepareStatement(sql);
+      rs = pstat.executeQuery(sql);
+
+      // rs에서 데이터 읽기
+      while (rs.next()) {
+        int id = rs.getInt("id");
+        LocalDateTime regDate = rs.getTimestamp("regDate").toLocalDateTime();
+        LocalDateTime updateDate = rs.getTimestamp("updateDate").toLocalDateTime();;
+        String subject = rs.getString("subject");
+        String content = rs.getString("content");
+
+        Article article = new Article(id, regDate, updateDate, subject, content);
+        articles.add(article);
+      }
+      
+      System.out.println("결과 : " + articles);
+
+    } catch (ClassNotFoundException e) {
+      System.err.println("JDBC 드라이버를 찾지 못했습니다.");
+      e.printStackTrace();
+    } catch (SQLException e) {
+      System.out.println("데이터 베이스 연결 실패");
+      e.printStackTrace();
+    } finally {
+      try {
+        if(rs != null && !rs.isClosed()) rs.close();
+        if(pstat != null && !pstat.isClosed()) pstat.close();
+        if(conn != null && !conn.isClosed()) {
+          conn.close(); // 데이터 베이스 연결 해제
+          System.out.println("데이터베이스 연결이 해제되었습니다.");
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
