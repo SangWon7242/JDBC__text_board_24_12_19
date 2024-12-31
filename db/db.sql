f7b677cf148f15fe1ef7bb6206fbaec9ab0e5fbe
SangWon7242 <gotkdqja@gmail.com>
2024-12-31 21:19:14 +0900

    댓글 테이블 생성

--- db/db.sql
+++ db/db.sql
@@ -25,6 +25,16 @@ CREATE TABLE `member` (
 # article 테이블에 memberId 추가
 ALTER TABLE article ADD COLUMN memberId INT UNSIGNED NOT NULL AFTER updateDate;
 
+# reply테이블 생성
+CREATE TABLE reply (
+	id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	memberId INT UNSIGNED NOT NULL,
+	articleId INT UNSIGNED NOT NULL,
+	content TEXT NOT NULL
+);
+
 # 회원 테스트 데이터
 INSERT INTO `member`
 SET regDate = NOW(),
@@ -55,4 +65,19 @@ memberId = 2,
 `subject` = '제목2',
 content = '내용2';
 
-SELECT * FROM article;
+# 댓글 테스트 데이터
+# 1번 회원이 1번 글에 대한 댓글
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 1,
+articleId = 1,
+content = '내용에 대한 답변 1';
+
+# 2번 회원이 1번 글에 대한 댓글
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+articleId = 1,
+content = '내용에 대한 답변 2';
