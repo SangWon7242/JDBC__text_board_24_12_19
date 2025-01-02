2339f559cbfb5734b46b0171440d117702b7c7bb
SangWon7242 <gotkdqja@gmail.com>
2025-01-02 21:39:22 +0900

    SQL 업데이트

--- db/db.sql
+++ db/db.sql
@@ -72,6 +72,8 @@ hit = 15;
 
 SELECT COUNT(*) FROM article;
 
+SELECT * FROM article;
+
 # 다량의 게시물 테스트 데이터(개수 증가)
 INSERT INTO article (regDate, updateDate, memberId, `subject`, content, hit)
 SELECT
@@ -82,3 +84,50 @@ CONCAT('제목-', UUID()),
 CONCAT('내용-', UUID()),
 CAST(10 + (30-10) * RAND() AS INT)
 FROM article;
+
+# 댓글 테스트 데이터
+# 1번 회원이 1번 글에 대해 답변
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 1,
+articleId = 1, # 1번 글에 대한 답변
+content = '1번 게시물에 대한 댓글 1';
+
+# 2번 회원이 1번 글에 대해 답변
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+articleId = 1, # 1번 글에 대한 답변
+content = '1번 게시물에 대한 댓글 2';
+
+# 게시물 작성자의 이름을 구하기 위한 SQL
+SELECT A.*,
+M.`name` AS `extra__writerName`
+FROM article AS A
+INNER JOIN `member` AS M
+ON A.memberId = M.id
+
+# 댓글 작성자의 이름을 구하기 위한 SQL
+SELECT R.*,
+M.`name` AS `extra__writerName`
+FROM reply AS R
+INNER JOIN `member` AS M
+ON R.memberId = M.id
+
+# 1번 게시물에 대한 댓글
+SELECT R.*,
+M.`name` AS `extra__writerName`
+FROM reply AS R
+INNER JOIN `member` AS M
+ON R.memberId = M.id
+WHERE R.articleId = 1
+ORDER BY R.id DESC;
+
+# 해당 게시물에 댓글이 달려 있는지 여부 확인
+SELECT COUNT(*) > 0
+FROM reply AS R
+INNER JOIN `member` AS M
+ON R.memberId = M.id
+WHERE R.articleId = 2
